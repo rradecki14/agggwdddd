45c49521fa96cde29e98751536cb340745351c5e
rradecki14 <rradecki14@gmail.com>
2026-04-07 12:29:06 -0700

    Ensure script runs from its directory
    Add script to navigate to its directory before execution

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+# Navigate to script directory to ensure relative paths work
+cd "$(dirname "$0")"
+
 pip install -r requirements.txt
 uvicorn main:app --host 0.0.0.0 --port 10000
